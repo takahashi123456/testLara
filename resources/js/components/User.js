2e4e4942e3f9d3ac36c307d2973ab7dcf61ef279
Inazawa <takahashinishikigoi@gmail.com>
2021-09-12 23:15:12 +0900

    database import して見えるようにした

--- resources/js/components/User.js
+++ resources/js/components/User.js
@@ -1,8 +1,27 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { divide } from 'lodash';
 
 function User() {
-    return <h1> User page</h1>;
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        getUsers()
+    }, [])
+
+    const getUsers = async () => {
+        const response = await axios.get('/api/user');
+        setUsers(response.data.users)
+    }
+
+    return (
+        <div>
+            <h1> User page</h1>
+            <ul>
+                {users.map((user) => <li key="[user.id]">{user.name}</li>)}
+            </ul>
+        </div>
+    );
+
 }
 
 export default User;
